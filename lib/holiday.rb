f3c89d20af223ae40d00cbc01556d2e3a4bf5630
Zachary Durr <zdurr22@gmail.com>
2020-10-16 19:23:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,6 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_hash.each do |season, holiday_names_hash|
   season == :winter ? holiday_names_hash.each do |holiday_name, supplies|
-
     puts supplies
 end
 
